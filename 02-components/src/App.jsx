1309c228a5660b87e6292037a711d41c4acff117
BhavishyaTawar <bhavishyatawar00@gmail.com>
2025-11-09 20:44:05 +0530

    components basics

---
+++ 02-components/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Card from './components/card.jsx'
+const App = () => {
+  return (
+    <div>
+      <h1> Hello from App Component </h1>
+      <Card />
+      <nav className="nav">Navigation Bar</nav>
+    </div>
+  
+  )
+}
+
+  
+
+export default App
---
+++ 02-components/src/components/nac.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const nac = () => {
+  return (
+    <div>
+        <h1>nac Component</h1>
+        <p>This is a simple nac component.</p>
+    </div>
+  )
+}
+
+export default nac
